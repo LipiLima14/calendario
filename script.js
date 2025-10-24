258ca758e74634a628d30a10a4ef1d021519a8b5
Filipi <144926216+LipiLima14@users.noreply.github.com>
2025-10-24 14:39:13 -0300

    Editing script.js file

--- script.js
+++ script.js
@@ -1,23 +1,55 @@
-const CALENDARIO = document.querySelector('.calendario')
-const ITEM = document.querySelectorAll('.calendario .item')
-const NUM_COLUMNS = 5
-const NUM_ROWS = 7
+const CALENDARIO = document.querySelector('.calendario');
+const ITENS = document.querySelectorAll('.calendario .item');
+const ANO = document.querySelector('.ano-atual')
+const MES = document.querySelector('.mes-atual')
 
+function organizar_itens() {
+    const COLUNA_GRID = 7; 
+    ITENS.forEach((item, index) => {
+        let linha_inicial = Math.floor(index / COLUNA_GRID) + 1;
+        let coluna_inicial = (index % COLUNA_GRID) + 1;
+        item.style.gridArea = `${linha_inicial} / ${coluna_inicial} / ${linha_inicial + 1} / ${coluna_inicial + 1}`;
+    });
+}
 
-function organizar_items() {
-    let row_start = 0
-    let column_start = 0
-    let row_end = 1
-    let column_end = 1
-    for (let i = 0; i < NUM_ROWS; i++) {
-        row_start++
-        row_end++
-        for (let i2 = 0; i < NUM_COLUMNS; i2++) {
-            column_start++
-            column_end++
-            ITEM.forEach(item => {
-                item.style.gridArea = `${row_start} / ${column_start} / ${row_end} / ${column_end}`
-            })
+organizar_itens();
+
+let data = new Date()
+let dia_do_mes = data.getDate()
+let dia_da_semana = data.getDay()
+let mes_atual = data.getMonth()
+let ano_atual = data.getFullYear()
+let primeiro_dia_do_mes = new Date(ano_atual, mes_atual, 1)
+let ultimo_dia_do_mes = new Date(ano_atual, mes_atual + 1, 0).getDate()
+let primeiro_dia_da_semana_do_mes = primeiro_dia_do_mes.getDay()
+
+let lista_itens = Array.from(ITENS)
+
+function setar_dias_do_mes(lista, dia_inicial, ultimo_dia, dia) {
+    let numero = 1
+    for (let i = dia_inicial; i < (ultimo_dia + dia_inicial); i++) {
+        if (numero === dia) {
+            lista[i].style.backgroundColor = '#ec4d4d'
+            lista[i].style.color = '#fff'
         }
+        if (numero < 10) {
+            lista[i].textContent = `0${numero}`
+        } else {
+            lista[i].textContent = numero
+        }
+        numero++
     }
 }
+
+setar_dias_do_mes(lista_itens, primeiro_dia_da_semana_do_mes, ultimo_dia_do_mes, dia_do_mes)
+
+function desenhar_mes_e_ano(ano, mes) {
+    const nomesDosMeses = [
+        'JANEIRO', 'FEVEREIRO', 'MARÇO', 'ABRIL', 'MAIO', 'JUNHO', 'JULHO', 'AGOSTO', 'SETEMBRO', 'OUTUBRO', 'NOVEMBRO', 'DEZEMBRO'
+    ];
+    
+    ANO.textContent = ano;
+    MES.textContent = nomesDosMeses[mes];
+}
+
+desenhar_mes_e_ano(ano_atual, mes_atual);
